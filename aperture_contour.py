75b05ee7ceb380db8612d843e17c96891e2be878
Tajiri Tomoyuki <tomoyuki-tajiri@eps.s.u-tokyo.ac.jp>
2019-12-04 15:04:10 +0900

    translate in English

--- aperture_contour.py
+++ aperture_contour.py
@@ -1,28 +1,42 @@
 #coding:utf-8
 import numpy as np
 
-#4方向すべて試してみて進める方向があるか確認
+class ApertureContourException(Exception):
+    pass
+
+def is_valid_aperture(aperture):
+    if not isinstance(aperture, np.ndarray):
+        raise ApertureContourException("aperture must be a numpy.ndarray class")
+    if aperture.dtype not in (np.int8, np.int16, np.int32, np.int64, np.uint8, np.uint16, np.uint32, np.uint64, np.bool):
+        raise ApertureContourException("data type of aperture must be a bool or int")
+    if not aperture.ndim == 2:
+        raise ApertureContourException("aperture must be 2 dimensional data")
+    return aperture
+
+
+# Try all four directions to check if there is a direction to proceed
 def go_point(point, points):
     directions = [(0.5, 0), (0, 0.5), (-0.5, 0), (0, -0.5)]
-    #各方向に進めるか確認
+    #Try each directions
     for direction in directions:
         point_cand = point + direction
         is_point = np.all(points == point_cand, axis=1)
-        #もし進めるなら
+        #if proceed
         if np.sum(is_point) != 0:
-            #もとの点を削除
+            #delete previous points
             indexes = np.where(is_point)
             points = np.delete(points, indexes[0][0], 0)
             return point_cand, points
-    #どこにも進めない場合、Noneを返す
+    #If can't go anywhere, return None
     else:
         return None, points
 
 def make_contours(aperture):
-    #apertureの各点の座標を求める
-    h, w = np.where(aperture == 1)
+    aperture = is_valid_aperture(aperture)
+    # the coordinates of each point of the aperture
+    h, w = np.where(aperture != 0)
     pixs = np.vstack((w, h)).T
-    #各apertureの端の点と辺の点を求める
+    # the edge and side points of each coordinates
     TR = pixs + np.array([-0.5, -0.5])
     TL = pixs + np.array([0.5, -0.5])
     BR = pixs + np.array([-0.5, 0.5])
@@ -33,33 +47,32 @@ def make_contours(aperture):
     B = pixs + np.array([0, 0.5])
     points_edge = np.vstack((TR, TL, BR, BL))
     points_side = np.vstack((T, B, R, L))
-    #重複を削除したものを作成
+    # remove duplicate
     points_edge_unique = np.unique(points_edge, axis=0)
     points_side_unique = np.unique(points_side, axis=0)
-    #apertureを構成する点ではないものを削除
+    # removed points which are not part of the aperture contour
     points_edge_all = np.array([point for point in points_edge_unique if np.sum(np.all(points_edge == point, axis=1)) != 4])
     points_side_all = np.array([point for point in points_side_unique if np.sum(np.all(points_side == point, axis=1)) != 2])
-    #crossする点は重複カウントする
-    #crossする点は2つのpixelのedgeを共有
+    # the crossing point shares the edge of two pixels, so it counts twice
     points_cross_cand = [point for point in points_edge_all if np.sum(np.all(points_edge == point, axis=1)) == 2]
-    #crossする点は近傍4辺を持つ
+    # the crossing point has 4 side points
     points_cross_all = np.array([point for point in points_cross_cand if np.sum(np.all(np.abs(points_side_all - point) <= 0.5, axis=1)) == 4])
-    #まとめる
+    # stack all points
     if points_cross_all.shape[0] == 0:
         points_all = np.vstack((points_edge_all, points_side_all))
     else:
         points_all = np.vstack((points_edge_all, points_side_all, points_cross_all))
-    #lineを作る点を並べる
+    # line up points which consist contour
     lines = []
-    #apertureは一つとは限らないので、全てのaperutreを経由できているか確認
+    # Since the contours are not limited to one, check all separated contours
     while points_all.shape[0] != 0:
         point = points_all[0]
         contour = []
-        #一つのaperuter内の輪郭を引く
+        # draw one contour
         while point is not None:
             contour.append(point)
             point, points_all = go_point(point, points_all)
-        #折れ曲がり点のみ抽出
+        #Extract edge points
         contour_arr = np.array(contour)
         diffs = np.diff(contour_arr, 2, axis=0)
         diffs = np.vstack((np.zeros(2), diffs, np.zeros(2)))
