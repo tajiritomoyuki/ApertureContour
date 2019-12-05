cbbb7dc34ee58de73b392af9809f367eec707c98
Tajiri Tomoyuki <tomoyuki-tajiri@eps.s.u-tokyo.ac.jp>
2019-12-05 14:24:06 +0900

    bugfix

--- aperture_contour/__init__.py
+++ aperture_contour/__init__.py
@@ -1,6 +1,6 @@
 #-*-coding:utf-8-*-
 from .aperture_contour import draw_contours, contour_points
-from .sample import SampleImage
+from .sample import SampleImages
 __version__ = "1.0"
 __description__ = "draw aperture contours"
 __author__ = "Tomoyuki Tajiri"
@@ -11,6 +11,6 @@ __url__ = "https://github.com/tajiritomoyuki/aperture_contour"
 __all__ = [
     "contour_points",
     "draw_contours",
-    "SampleImage",
+    "SampleImages",
     "__version__",
 ]
